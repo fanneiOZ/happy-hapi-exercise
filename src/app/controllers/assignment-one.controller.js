a9103196208c845b195c116ca93eac59a837789b
Panya Batphantana <fanneioz@yahoo.co.th>
2020-08-29 23:46:02 +0700

    ✨ Assignment 1 - Hierarchy transformer implementations

--- src/app/controllers/assignment-one.controller.js
+++ src/app/controllers/assignment-one.controller.js
@@ -1,7 +1,12 @@
 import { Controller } from '../../libs/common/controller'
+import {
+    HierarchyTransformerService,
+} from '../../cores/hierarchy-transformer/hierarchy-transformer.service'
 
 export class AssignmentOneController extends Controller {
-    async handle() {
-        return { result: 'Hello' }
+    async handle(req) {
+        const { payload } = req
+
+        return new HierarchyTransformerService().execute(payload)
     }
 }
